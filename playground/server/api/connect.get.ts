781a8a5ebe40ecf5b93e255c2e21bb7c2da78ed2
Leonardo Vizcaya Savchenko <lvizcaya@arpixnet.com>
2025-04-18 10:32:39 -0500

    Update connect.get.ts

--- playground/server/api/connect.get.ts
+++ playground/server/api/connect.get.ts
@@ -1,10 +1,6 @@
 export default defineEventHandler(async () => {
   const db = useDbConnector()
   const token = ''
-  const data = await db.get('roly_config', { select: `
-    phone_contact
-    email_contact
-    social_net
-` }, token)
+  const data = await db.get('roly_config', { select: ['phone_contact', 'email_contact', 'social_net'] }, token)
   return data
 })
